c7648bd99a1b69a4229607bd584866d5f2cae41f
saaaaalmon <116255045+saaaaalmon@users.noreply.github.com>
2022-10-24 14:28:15 +0800

    Create datasets.py

---
+++ source/config/datasets.py
@@ -0,0 +1,14 @@
+import numpy as np
+import sys
+
+class BP4D(object):
+    dataset_path = './data/BP4D/list'
+    image_path = '../BP4D_crop/'
+    out_dir = './result/BP4D'
+    num_AU_points = 12
+
+class DISFA(object):
+    dataset_path = './data/DISFA/list'
+    image_path = '../DISFA_crop/'
+    out_dir = './result/DISFA'
+    num_AU_points = 8
